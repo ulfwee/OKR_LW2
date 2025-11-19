b085fba91ff927efa8d65269a4b900ebe9d00823
Sasha <sasha342008@gmail.com>
2025-11-19 16:01:27 +0200

    Design and test for Form1 added

---
+++ QuizApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using QuizApp;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Form1_Should_Open_Without_Exception()
+        {
+            var exception = Record.Exception(() =>
+            {
+                var form = new Form1();
+                form.Dispose();
+            });
+
+            Assert.Null(exception);
+        }
+
+    }
+}
